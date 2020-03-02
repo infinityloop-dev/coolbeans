35151258ff4b65434ed81061d86b8f241d7ddb51
Václav Pelíšek <peldax@gmail.com>
2020-03-02 23:05:32 +0100

    Create ArrayPrimaryKey.php

---
+++ src/CoolBeans/PrimaryKey/ArrayPrimaryKey.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\CoolBeans\PrimaryKey;
+
+class ArrayPrimaryKey implements PrimaryKey
+{
+    private array $value;
+
+    public function __construct(array $key)
+    {
+        $this->value = $key;
+    }
+
+    public function getValue() : array
+    {
+        return $this->value;
+    }
+
+    public function getName() : string
+    {
+        return \implode('|', \array_keys($this->value));
+    }
+}
