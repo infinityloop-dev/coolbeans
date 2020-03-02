5bb3f535389ccad37e5d9b9d0c1a82f99214440e
Václav Pelíšek <peldax@gmail.com>
2020-03-03 00:15:34 +0100

    Create Row.php

---
+++ src/CoolBeans/Row.php
@@ -0,0 +1,147 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\CoolBeans;
+
+abstract class Row implements \ArrayAccess, \IteratorAggregate
+{
+    use \Nette\SmartObject;
+
+    public ?\Infinityloop\CoolBeans\PrimaryKey\PrimaryKey $primaryKey;
+    protected \Nette\Database\Table\ActiveRow $row;
+    protected \ReflectionClass $reflection;
+
+    final public function __construct(\Nette\Database\Table\ActiveRow $row)
+    {
+        $this->row = $row;
+        $this->reflection = new \ReflectionClass(static::class);
+
+        if (\App\Bootstrap::isDebugMode()) {
+            $this->validateTableName();
+            $this->validateMissingColumns();
+        }
+
+        $this->initiateProperties();
+    }
+
+    public function getTableName() : string
+    {
+        return $this->row->getTable()->getName();
+    }
+
+    public function getIterator() : \Traversable
+    {
+        return $this->row->getIterator();
+    }
+
+    public function toArray() : array
+    {
+        return $this->row->toArray();
+    }
+
+    public function offsetGet($offset)
+    {
+        try {
+            $property = $this->reflection->getProperty($offset);
+
+            if ($property->isPublic()) {
+                return $this->{$offset};
+            }
+        } catch (\ReflectionException $e) {
+        }
+
+        throw new \Nette\InvalidStateException('Column not defined.');
+    }
+
+    public function offsetExists($offset) : bool
+    {
+        try {
+            $property = $this->reflection->getProperty($offset);
+
+            return $property->isPublic();
+        } catch (\ReflectionException $e) {
+            return false;
+        }
+    }
+
+    public function offsetSet($offset, $value) : void
+    {
+        throw new \Nette\InvalidStateException();
+    }
+
+    public function offsetUnset($offset) : void
+    {
+        throw new \Nette\InvalidStateException();
+    }
+
+    protected function ref(string $key, ?string $throughColumn = null) : ?\Nette\Database\Table\ActiveRow
+    {
+        return $this->row->ref($key, $throughColumn);
+    }
+
+    protected function related(string $key, ?string $throughColumn = null) : \Nette\Database\Table\GroupedSelection
+    {
+        return $this->row->related($key, $throughColumn);
+    }
+
+    protected function validateTableName() : void
+    {
+        $tableName = $this->getTableName();
+        $className = $this->reflection->getShortName();
+        $sepIndex = \Nette\Utils\Strings::indexOf($tableName, '.');
+
+        if (\is_int($sepIndex)) {
+            $tableName = \Nette\Utils\Strings::substring($tableName, $sepIndex + 1);
+        }
+
+        if ($tableName !== \Infinityloop\Utils\CaseConverter::toSnakeCase($className)) {
+            throw new \Nette\InvalidStateException('Provided ActiveRow table doesnt match.');
+        }
+    }
+
+    protected function validateMissingColumns() : void
+    {
+        foreach ($this->row->toArray() as $name => $value) {
+            if (!$this->offsetExists($name)) {
+                throw new \App\Infinityloop\CoolBeans\Exception\MissingProperty(static::class . ': ' . $name);
+            }
+        }
+    }
+
+    protected function initiateProperties() : void
+    {
+        $this->primaryKey = \Infinityloop\CoolBeans\PrimaryKey\PrimaryKey::create($this->row);
+
+        foreach ($this->reflection->getProperties(\ReflectionProperty::IS_PUBLIC) as $property) {
+            $name = $property->getName();
+            $type = $property->getType();
+            $value = $this->row[$name];
+
+            if ($name === 'primaryKey') {
+                continue;
+            }
+
+            if (!$type instanceof \ReflectionType) {
+                throw new \App\Infinityloop\CoolBeans\Exception\MissingType($property->getName() . ' does not have type.');
+            }
+
+            switch ($type->getName()) {
+                case 'bool':
+                    $this->{$name} = \is_bool($value)
+                        ? $value
+                        : $value === 1;
+
+                    break;
+                case \Infinityloop\Utils\Json::class:
+                    $this->{$name} = \Infinityloop\Utils\Json::fromString($value);
+
+                    break;
+                default:
+                    $this->{$name} = $value;
+
+                    break;
+            }
+        }
+    }
+}
