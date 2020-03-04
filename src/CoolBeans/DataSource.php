cfb72be603c33d4c6b871bc0405133386c4fd290
Václav Pelíšek <peldax@gmail.com>
2020-03-05 00:24:54 +0100

    Changed abstract dataSource to interface

--- src/CoolBeans/DataSource.php
+++ src/CoolBeans/DataSource.php
@@ -6,54 +6,45 @@ namespace Infinityloop\CoolBeans;
 
 use Infinityloop\CoolBeans\PrimaryKey\PrimaryKey;
 
-abstract class DataSource
+interface DataSource
 {
     /**
      * Returns table name
      */
-    abstract public function getName() : string;
+    public function getName() : string;
 
     /**
      * Returns row from table by its id
      */
-    abstract public function getRow(PrimaryKey $key);
+    public function getRow(PrimaryKey $key);
 
     /**
      * Returns selection of all entries
      */
-    abstract public function findAll() : \Iterator;
+    public function findAll() : \Iterator;
 
     /**
      * Returns selection of entries found by associative array
      */
-    abstract public function findByArray(array $filter) : \Iterator;
+    public function findByArray(array $filter) : \Iterator;
 
     /**
      * Inserts data into table
      */
-    abstract public function insert(array $data) : PrimaryKey;
+    public function insert(array $data) : PrimaryKey;
 
     /**
      * Updates row
      */
-    abstract public function update(PrimaryKey $key, array $data) : PrimaryKey;
+    public function update(PrimaryKey $key, array $data) : PrimaryKey;
 
     /**
      * Deletes row
      */
-    abstract public function delete(PrimaryKey $key) : void;
+    public function delete(PrimaryKey $key) : void;
 
     /**
      * Inserts data if no key provided, updates otherwise
      */
-    public function upsert(?PrimaryKey $key, array $values) : PrimaryKey
-    {
-        if ($key instanceof PrimaryKey) {
-            $this->update($key, $values);
-
-            return $key;
-        }
-
-        return $this->insert($values);
-    }
+    public function upsert(?PrimaryKey $key, array $values) : PrimaryKey;
 }
