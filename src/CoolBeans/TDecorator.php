95132fa8c8c9b49c98557fb1527961c84af80077
Václav Pelíšek <peldax@gmail.com>
2020-03-05 02:32:42 +0100

    Create TDecorator.php

---
+++ src/CoolBeans/TDecorator.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\CoolBeans;
+
+use Infinityloop\CoolBeans\PrimaryKey\PrimaryKey;
+
+trait TDecorator
+{
+    protected DataSource $dataSource;
+    
+    public function getName() : string
+    {
+        return $this->dataSource->getName();
+    }
+    
+    public function getRow(PrimaryKey $key)
+    {
+        return $this->dataSource->getRow($key);
+    }
+    
+    public function findAll() : \Iterator
+    {
+        return $this->dataSource->findAll();
+    }
+    
+    public function findByArray(array $filter) : \Iterator
+    {
+        return $this->dataSource->findByArray($filter);
+    }
+    
+    public function insert(array $data) : PrimaryKey
+    {
+        return $this->dataSource->insert($data);
+    }
+    
+    public function insertMultiple(array $data) : array
+    {
+        return $this->dataSource->insertMultiple($data);
+    }
+    
+    public function update(PrimaryKey $key, array $data) : PrimaryKey
+    {
+        return $this->dataSource->update($key, $data);
+    }
+    
+    public function updateByArray(array $filter, array $data) : int
+    {
+        return $this->dataSource->updateByArray($filter, $data);
+    }
+
+    public function delete(PrimaryKey $key) : void
+    {
+        $this->dataSource->delete($key);
+    }
+
+    public function deleteByArray(array $filter) : int
+    {
+        return $this->dataSource->deleteByArray($filter);
+    }
+
+    public function upsert(?PrimaryKey $key, array $values) : PrimaryKey
+    {
+        return $this->dataSource->upsert($key, $values);
+    }
+}
