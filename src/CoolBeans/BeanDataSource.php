809f142b18e07d934edad21019d3a39fd507a165
Václav Pelíšek <peldax@gmail.com>
2020-03-06 04:27:47 +0100

    Create BeanDataSource.php

---
+++ src/CoolBeans/BeanDataSource.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\CoolBeans;
+
+use Infinityloop\CoolBeans\PrimaryKey\PrimaryKey;
+
+interface BeanDataSource extends \Infinityloop\CoolBeans\DataSource
+{
+    public function getRow(PrimaryKey $key) : \Infinityloop\CoolBeans\Bean;
+
+    public function findAll() : \Infinityloop\CoolBeans\Selection;
+
+    public function findByArray(array $filter) : \Infinityloop\CoolBeans\Selection;
+}
