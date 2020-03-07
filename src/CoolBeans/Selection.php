03efd28eee5bb586a973dbbe84777de45fa13ff1
Václav Pelíšek <peldax@gmail.com>
2020-03-07 16:17:05 +0100

    Update Selection.php

--- src/CoolBeans/Selection.php
+++ src/CoolBeans/Selection.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Infinityloop\CoolBeans;
 
-abstract class Selection implements \Iterator, \Countable
+abstract class Selection implements \Infinityloop\CoolBeans\Contract\Selection
 {
     use \Nette\SmartObject;
 
