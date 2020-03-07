1cd027a40cb3188a580e221e66b3ff363b2c2518
Václav Pelíšek <peldax@gmail.com>
2020-03-07 22:05:25 +0100

    Update and rename src/CoolBeans/NetteDataSource.php to src/CoolBeans/Bridge/Nette/DataSource.php

---
+++ src/CoolBeans/Bridge/Nette/DataSource.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\CoolBeans;
+
+use Infinityloop\CoolBeans\PrimaryKey\PrimaryKey;
+
+interface DataSource extends \Infinityloop\CoolBeans\Contract\DataSource
+{
+    public function getRow(PrimaryKey $key) : \Infinityloop\CoolBeans\Bridge\Nette\ActiveRow;
+
+    public function findAll() : \Infinityloop\CoolBeans\Bridge\Nette\Selection;
+
+    public function findByArray(array $filter) : \Infinityloop\CoolBeans\Bridge\Nette\Selection;
+}
