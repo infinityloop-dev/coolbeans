10ff559407ff4da9917349e3ee7e68195a49594a
Václav Pelíšek <peldax@gmail.com>
2020-03-07 14:54:10 +0100

    Create Row.php

---
+++ src/CoolBeans/Contract/Row.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\CoolBeans\Contract;
+
+interface Row extends \ArrayAccess
+{
+    public function toArray() : array;
+
+    public function getPrimaryKey() : \Infinityloop\CoolBeans\PrimaryKey\PrimaryKey;
+
+    public function __get(string $name);
+
+    public function __isset($name);
+}
