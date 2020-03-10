171f9a209de779c07eefaac2f4e5112f2dd452c9
Václav Pelíšek <peldax@gmail.com>
2020-03-10 22:37:32 +0100

    Update InsertMultiple.php

--- src/CoolBeans/Result/InsertMultiple.php
+++ src/CoolBeans/Result/InsertMultiple.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Infinityloop\CoolBeans\Result;
 
-class InsertMultiple
+class InsertMultiple implements \Infinityloop\CoolBeans\Contract\Result
 {
     use \Nette\SmartObject;
 
