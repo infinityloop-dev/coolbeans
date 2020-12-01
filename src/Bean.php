58ad4d4bb791b158f1ae6ecadeec7019c75ba89c
Vossik <vosik.m@seznam.cz>
2020-12-01 12:38:22 +0100

    codestyle

--- src/Bean.php
+++ src/Bean.php
@@ -24,7 +24,7 @@ abstract class Bean implements \CoolBeans\Contract\Row, \IteratorAggregate
             $this->validateMissingColumns();
         }
 
-        if(\CoolBeans\Config::$validateTableName){
+        if (\CoolBeans\Config::$validateTableName){
             $this->validateTableName();
         }
 
