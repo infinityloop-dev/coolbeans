9353a75340c32bb3ccf8d3cfd4d503af4bae51a7
Vossik <vosik.m@seznam.cz>
2020-12-09 14:46:12 +0100

    replaced contributte/console with symfony/console

--- src/Command/SqlGeneratorCommand.php
+++ src/Command/SqlGeneratorCommand.php
@@ -4,8 +4,6 @@ declare(strict_types = 1);
 
 namespace CoolBeans\Command;
 
-use Symfony\Component\Console\Input\InputArgument;
-
 class SqlGeneratorCommand extends \Symfony\Component\Console\Command\Command
 {
     private const INDENTATION = '    ';
