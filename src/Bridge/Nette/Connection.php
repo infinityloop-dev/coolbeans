274eb1c0980af9dc1544ee4b0a532f0ce31fe50d
Václav Pelíšek <peldax@gmail.com>
2022-10-26 20:06:39 +0000

    Unpack arguments.

--- src/Bridge/Nette/Connection.php
+++ src/Bridge/Nette/Connection.php
@@ -22,12 +22,12 @@ final class Connection extends \Nette\Database\Connection
     public function query(string $sql, ...$params) : \Nette\Database\ResultSet
     {
         try {
-            return parent::query($sql, $params);
+            return parent::query($sql, ...$params);
         } catch (\PDOException $e) {
             if ($e->getCode() === 'HY000' && \str_contains($e->getMessage(), 'gone away')) {
                 $this->reconnect();
 
-                return parent::query($sql, $params); // retry
+                return parent::query($sql, ...$params); // retry
             }
 
             throw $e;
